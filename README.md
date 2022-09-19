0895dbfd64bb7404cc2442872a806afdea88121b
ℭαጠყթαӥ_ß_Ŧαዙҝҿ <gunko.vlad.21.09.2001a@gmail.com>
2022-09-19 14:31:12 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Blog_PHP
+# php-blog
+Simple PHP OOP blog based on mvc
---
+++ application/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'host' => 'localhost',
+	'name' => 'diplom-prom-db',
+	'user' => 'root',
+	'password' => 'root',
+];
